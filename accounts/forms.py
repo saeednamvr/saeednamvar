36f2fe36cf12a4d58abb34f18554b19e8c115127
saeednamvr <sanrir3@gmail.com>
2022-08-27 13:25:12 +0430

    Revert "Update models.py"
    This reverts commit 59dff41e45bb0bac7882acfbb4093ecbccf899d6.

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,3 +4,10 @@ class UserRegisterationForm(forms.Form):
     username = forms.CharField()
     email =forms.EmailField()
     password = forms.CharField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .forms import UserRegisterationForm
+from .forms import UserRegisterationForm, UserLoginForm
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 def user_register(request):
@@ -9,7 +11,35 @@ def user_register(request):
         form = UserRegisterationForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create.user(cd['username'], cd['email'], cd['password'])
+            user =  User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            user.first_name = cd['first_name']
+            user.last_name = cd['last_name']
+            user.save()
+            messages.success(request,'user registerd successfuly', 'success')
+            return redirect('home')
     else:
         form = UserRegisterationForm()
     return render(request, 'register.html', {'form':form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request,'loged in successfuly', 'success')
+                return redirect('home')
+            else:
+                messages.error(request, 'username or password is wrong', 'danger')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form':form})
+
+def user_logout(requset):
+    logout(requset)
+    messages.success(requset, 'logot successfully', 'success')
+    return redirect('home')
+
+
